bdb1e18ffec5c0c4c2fd2660e2b1dcd0a95d72f7
daynewlee331 <liyi64125837@163.com>
2016-04-14 17:20:07 -0400

    getting started with py file

---
+++ pre_process_data.py
@@ -0,0 +1,38 @@
+import csv
+import pandas as pd
+import numpy as np
+import json
+from collections import defaultdict
+
+def readJson(f):
+    for l in open(f):
+        yield eval(l)
+
+items = []
+
+for l in open("anonymous-msweb.data"):
+    if l.startswith("A"):
+        m,n,p,q,i = l.strip().split(',')
+        q = q[1:-1]
+        i = i[1:-1]
+        tmp = [n, q, i]
+        items.append(tmp)
+
+items_df = pd.DataFrame(items)
+items_df.to_csv("item.txt")
+
+current = ""
+item = ""
+user_item = []
+for l in open("anonymous-msweb.data"):
+    if l.startswith("C"):
+        m,n,p = l.strip().split(',')
+        current = p
+    if l.startswith("V"):
+        a,b,c = l.strip().split(',')
+        item = b
+        tp = [current, item]
+        user_item.append(tp)
+    
+user_item_df = pd.DataFrame(user_item)
+user_item_df.to_csv("user_item.txt")
